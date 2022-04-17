9aebb4a59af28422510538c5328509e7f085050e
Alisson Melo <alissonmelo.work@gmail.com>
2022-04-17 20:35:13 -0300

    código das duas primeiras aulas

---
+++ src/zeldaminiclone/Player.java
@@ -0,0 +1,46 @@
+package zeldaminiclone;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Player extends Rectangle{
+
+	public int spd = 4;
+	public boolean right, left, up, down;
+	
+	public Player(int x, int y) {
+		super(x,y, 32, 32);
+	}
+	
+	public void tick() {
+
+		
+//		POSIÇÃO X
+		
+		if (right) {
+			x += spd;
+		}else if(left){
+			x -= spd;
+		}
+		
+//		POSIÇÃO Y
+		
+		if (up) {
+			y -= spd;
+		}else if(down){
+			y += spd;
+		}
+	}
+	
+	public void render() {
+		
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.blue);
+		g.fillRect(x, y, width, height);
+		
+	}
+	
+}
